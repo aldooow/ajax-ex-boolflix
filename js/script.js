7b31162dfde2946a8b5af7ffcf6ff9eff7b87206
Aldo Cahuana <aldooow@icloud.com>
2020-07-01 12:57:01 +0200

    Work in progress!!!

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// 8269547e83cd87d7a77b566ccaff24bd
+//  https://api.themoviedb.org/3/movie/550?api_key=8269547e83cd87d7a77b566ccaff24bd
+
+$(document).ready(
+  function(){
+
+
+    $("#js_button-search").on("click", function(){
+      $(".wrapper-film").text("");
+      var value = $("#js_search").val()
+      var queryValue = "'" + value + "'"
+      console.log(queryValue)
+      $.ajax(
+      {
+        url: 'https://api.themoviedb.org/3/search/movie',
+        method: 'GET',
+        data: {
+          api_key: '8269547e83cd87d7a77b566ccaff24bd',
+          query: queryValue
+        },
+        success: function(data){
+          console.log(data)
+          // Handlebars.
+          var source = $("#film-template").html();
+          var template = Handlebars.compile(source);
+
+          // Variante dei risultati del Inpunt dei Films
+          var films = data.results;
+          // Ciclo FOR con Lunghezza della Variante che contiene tutta la risposta URL.
+             for(var i = 0; i<films.length; i++){
+               // Variante di singolo Film che è dentro l'Array.
+               var film = data.results[i];
+               // TEMPLATE da Appendere.
+               var html = template(film);
+               // Appendere TEMPLATE HTML nel Elemento desiderato.
+               $(".wrapper-film").append(html);
+             }
+
+        },
+        error: function(){
+            alert('ERROR');
+        }
+        }
+    );
+    });
+
+
+
+  });
